5fce47545913ec56ba6cc09dd60539419c05c96f
saiful.islam <saiful.islam@RENT-MIS-LT3045.verse.in>
2019-05-19 23:45:59 +0530

    Hybrid Frame work project commit

---
+++ HyberidFrameWork/src/com/selenium/base/BaseClass.java
@@ -0,0 +1,205 @@
+package com.selenium.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import com.google.common.io.Files;
+
+
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	public static Map<String,String> locatormap= new HashMap<String,String>();
+	public static Map<String,String> inputmap= new HashMap<String,String>();
+	
+	
+	/*
+	 * public static void main(String[] args) throws IOException {
+	 * 
+	 * getDataFromExcelFile(); getLocatorDataFromExcelFile(); }
+	 */
+	 
+
+	public static String getConfigData(String key) throws IOException
+	{
+		String value="";
+		File f= new File("./src/data/config.properties");
+		InputStream iStream= new FileInputStream(f);
+		Properties prop=new Properties();
+		prop.load(iStream);
+		value=prop.getProperty(key);
+		
+		return value;
+	}
+	@BeforeSuite
+	public static void getLocatorDataFromExcelFile() throws IOException
+	{
+	
+		
+		File f= new File("./src/data/input.xlsx");
+		FileInputStream iStream= new FileInputStream(f);
+		XSSFWorkbook wb=new XSSFWorkbook(iStream);
+		XSSFSheet ws=wb.getSheet("Sheet1");
+		int row=ws.getLastRowNum();
+		
+		 System.out.println(row);
+		for(int i=1;i<=row;i++)
+			
+			  {
+		String	page=ws.getRow(i).getCell(0).getStringCellValue();
+		String	elementsname=ws.getRow(i).getCell(1).getStringCellValue();
+		String	data=ws.getRow(i).getCell(2).getStringCellValue();
+		inputmap.put(page+"$"+elementsname, data);
+		
+		
+			  System.out.println(" xpath ="+page+" "+elementsname+" "+data);
+			  
+			  } 
+			 		
+		wb.close();
+	}
+	
+	@BeforeSuite
+	public static void getDataFromExcelFile() throws IOException
+	{
+	
+		
+		File f= new File("./src/data/locatordata.xlsx");
+		FileInputStream iStream= new FileInputStream(f);
+		XSSFWorkbook wb=new XSSFWorkbook(iStream);
+		XSSFSheet ws=wb.getSheet("Sheet1");
+		int row=ws.getLastRowNum();
+		
+		 System.out.println(row);
+		for(int i=1;i<=row;i++)
+			
+			  {
+		String	page=ws.getRow(i).getCell(0).getStringCellValue();
+		String	elementsname=ws.getRow(i).getCell(1).getStringCellValue();
+		String	data=ws.getRow(i).getCell(2).getStringCellValue();
+		locatormap.put(page+"$"+elementsname, data);
+		
+		
+			  System.out.println("Data ="+page+" "+elementsname+" "+data);
+			  
+			  } 
+			 		
+		wb.close();
+	}
+	
+	public static void BrowserClose()
+	{
+		driver.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * public static String getLocatorData(String key) throws IOException { String
+	 * value=""; File f= new File("./src/data/locator.prperties"); InputStream
+	 * iStream= new FileInputStream(f); Properties prop=new Properties();
+	 * prop.load(iStream); value=prop.getProperty(key);
+	 * 
+	 * return value; }
+	 */
+	/*
+	 * public static String getInputData(String key) throws IOException { String
+	 * value=""; File f= new File("./src/data/inputdata.prperties"); InputStream
+	 * iStream= new FileInputStream(f); Properties prop=new Properties();
+	 * prop.load(iStream); value=prop.getProperty(key);
+	 * 
+	 * return value; }
+	 */
+	@BeforeTest
+	public static void LaunchBrowser() throws IOException 
+	{
+		System.out.println("Before suite run");
+		String browser= getConfigData("browser");
+		String url=getConfigData("url");
+		String timeout=getConfigData("timeout");
+		int tout=Integer.parseInt(timeout);
+		if(browser.equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "./utilities/chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(browser.equals("firefox"))
+			
+		{
+			System.setProperty("webdriver.gecko.driver", "./utilities/geckodriver.exe");
+			driver=new ChromeDriver();
+		}
+		else
+		{
+			System.out.println("Browser not supported");
+		}
+		
+driver.manage().timeouts().implicitlyWait(tout, TimeUnit.SECONDS);
+		
+		driver.get(url);
+	}
+	
+	public static void captureScreenShot(String fileName) throws IOException
+	{
+		
+
+		File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		File dest = new File("./src/results/screenshots/"+fileName+".png");
+		
+		Files.copy(source, dest);
+	}
+	
+	public static void writeResultsToFile(String testcasename,String status) throws IOException
+	{
+		
+		File f = new File("./src/results/results.txt");
+		
+		FileWriter fw = new FileWriter(f,true);
+		
+		fw.write(testcasename+"----> "+status +"\n");
+		
+		
+		fw.flush();
+		fw.close();
+	}
+	@AfterTest
+	public static void browserClose()
+	{
+		System.out.println("After suite");
+		BrowserClose();
+	}
+	 
+}
+
---
+++ HyberidFrameWork/src/com/selenium/test/Login.java
@@ -0,0 +1,339 @@
+package com.selenium.test;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import com.selenium.base.BaseClass;
+
+public class Login extends BaseClass {
+	
+/*static {
+		
+		try {
+			getDataFromExcelFile(); 
+			getLocatorDataFromExcelFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	*/
+
+	//public static void main(String[] args) throws IOException {
+	
+		
+	//}
+	@Test(priority=0)
+	public static void login_AT_001() throws IOException {
+		
+		System.out.println("Test case 1");
+		 //LaunchBrowser();
+		 boolean result = false;	
+		  try
+		  {
+		  driver.findElement(By.xpath(locatormap.get("Login$UserName_Editbox"))).
+		  sendKeys(inputmap.get("Login$UserName_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Password_Editbox"))).
+		  sendKeys(inputmap.get("Login$Password_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Login_Button"))).click();
+		//  BrowserClose();
+		  
+		  
+		  result = driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).isDisplayed();
+			
+			
+			
+			if (result)
+			{
+				System.out.println("Login_001 is passed");
+				writeResultsToFile("Login_001","PASS");
+			}
+			else
+			{
+				captureScreenShot("login_AT_001");
+				System.out.println("Login_001 is Failed!!");
+				writeResultsToFile("Login_001","FAILED!!!");
+			}
+		 }
+			catch(Exception e)
+			{
+				captureScreenShot("login_AT_001");
+				System.out.println("Login_001 is Failed!!");
+				writeResultsToFile("Login_001","FAILED!!!");
+			}
+		  finally
+		  {
+			  driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).click();
+		  }
+	}
+	@Test(priority=2)
+	public static void login_AT_002() throws IOException {
+		
+		System.out.println("Test case 2");
+	//	 LaunchBrowser();
+		 boolean result = false;	
+		 try {
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$UserName_Editbox"))).
+		  sendKeys(inputmap.get("Login$UserName_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Password_Editbox"))).
+		  sendKeys(inputmap.get("Login$Password_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Login_Button"))).click();
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Tasks_link"))).click();
+		 
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Add_New_Task"))).click();
+		 
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Add_New_Customer"))).click();
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Customer$CustomerName_Editbox"))).
+		  sendKeys(inputmap.get("Customer$CustomerName_Editbox"));
+	  
+		  driver.findElement(By.xpath(locatormap.get("Customer$CustomerDescription_TextArea"))).
+		  sendKeys(inputmap.get("Customer$CustomerDescription_TextArea"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Customer$CreateCustomer_Button"))).click();
+		  
+		//  BrowserClose();
+	  result = driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).isDisplayed();
+			
+			
+			
+			if (result)
+			{
+				System.out.println("Login_002 is passed");
+				writeResultsToFile("Login_002","PASS");
+			}
+			else
+			{
+				captureScreenShot("login_AT_002");
+				System.out.println("Login_002 is Failed!!");
+				writeResultsToFile("Login_002","FAILED!!!");
+			}
+		 }
+			catch(Exception e)
+			{
+				captureScreenShot("login_AT_002");
+				System.out.println("Login_002 is Failed!!");
+				writeResultsToFile("Login_002","FAILED!!!");
+			}
+			
+		 finally
+		  {
+			  driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).click();
+		  }
+			
+		//	BrowserClose();
+	}
+	@Test(priority=1)
+	public static void login_AT_003() throws IOException {
+		System.out.println("Test case 3");
+		
+		 //LaunchBrowser();
+		 boolean result = false;	
+		 try {
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$UserName_Editbox"))).
+		  sendKeys(inputmap.get("Login$UserName_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Password_Editbox"))).
+		  sendKeys(inputmap.get("Login$Password_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Login_Button"))).click();
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Tasks_link"))).click();
+		 
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Add_New_Task"))).click();
+		 
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Add_New_Project"))).click();
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$ProjectName_Editbox"))).
+		  sendKeys(inputmap.get("Project$ProjectName_Editbox"));
+		  
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$Select_Customer"))).click();
+		//  sendKeys(inputmap.get("Project$Select_Customer")).click();
+		  
+		   
+			
+			/*
+			 * WebElement dropdown= driver.findElement(By.xpath(
+			 * "//*[@id=\"createProjectPopup_content\"]/div[2]/div[1]/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[2]/div/div[1]/input"
+			 * ));
+			 * 
+			 * Select selec=new Select(dropdown); selec.getOptions();
+			 * selec.selectByIndex(1);
+			 * 
+			 * 
+			 */
+		    driver.findElement(By.xpath("//*[@id=\"createProjectPopup_content\"]/div[2]/div[1]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/div/div[2]")).click();
+		  
+		  
+		  
+		  
+		  
+		  
+			/*
+			 * WebElement dropdown=
+			 * driver.findElement(By.xpath("Customer$Select_Customer"));
+			 * 
+			 * 
+			 * Select comboSelect= new Select(dropdown);
+			 * comboSelect.deselectByVisibleText("JEC_Manage");
+			 * 
+			 * System.out.println(comboSelect.getOptions().size());
+			 */
+		  
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$ProjectDescription_EditBox"))).
+		  sendKeys(inputmap.get("Project$ProjectDescription_EditBox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$CreateProject_Button"))).click();
+		  
+		//  BrowserClose();
+	  result = driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).isDisplayed();
+			
+			
+			
+			if (result)
+			{
+				System.out.println("Login_003 is passed");
+				writeResultsToFile("Login_003","PASS");
+			}
+			else
+			{
+				captureScreenShot("login_AT_003");
+				System.out.println("Login_003 is Failed!!");
+				writeResultsToFile("Login_003","FAILED!!!");
+			}
+		 }
+			catch(Exception e)
+			{
+				captureScreenShot("login_AT_003");
+				System.out.println("Login_003 is Failed!!");
+				writeResultsToFile("Login_003","FAILED!!!");
+			}
+			
+		 finally
+		  {
+			  driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).click();
+		  }
+			
+		//	BrowserClose();
+	}
+	
+	public static void login_AT_004() throws IOException {
+		 LaunchBrowser();
+		 boolean result = false;	
+		 try {
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$UserName_Editbox"))).
+		  sendKeys(inputmap.get("Login$UserName_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Password_Editbox"))).
+		  sendKeys(inputmap.get("Login$Password_Editbox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Login$Login_Button"))).click();
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Tasks_link"))).click();
+		 
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Add_New_Task"))).click();
+		 
+		  
+		  driver.findElement(By.xpath(locatormap.get("Home$Add_New_Project"))).click();
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$ProjectName_Editbox"))).
+		  sendKeys(inputmap.get("Project$ProjectName_Editbox"));
+		  
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$Select_Customer"))).click();
+		//  sendKeys(inputmap.get("Project$Select_Customer")).click();
+		  
+		   
+			
+			/*
+			 * WebElement dropdown= driver.findElement(By.xpath(
+			 * "//*[@id=\"createProjectPopup_content\"]/div[2]/div[1]/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[2]/div/div[1]/input"
+			 * ));
+			 * 
+			 * Select selec=new Select(dropdown); selec.getOptions();
+			 * selec.selectByIndex(1);
+			 * 
+			 * 
+			 */
+		    driver.findElement(By.xpath("//*[@id=\"createProjectPopup_content\"]/div[2]/div[1]/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/div/div[2]")).click();
+		  
+		  
+		  
+		  
+		  
+		  
+			/*
+			 * WebElement dropdown=
+			 * driver.findElement(By.xpath("Customer$Select_Customer"));
+			 * 
+			 * 
+			 * Select comboSelect= new Select(dropdown);
+			 * comboSelect.deselectByVisibleText("JEC_Manage");
+			 * 
+			 * System.out.println(comboSelect.getOptions().size());
+			 */
+		  
+		  
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$ProjectDescription_EditBox"))).
+		  sendKeys(inputmap.get("Project$ProjectDescription_EditBox"));
+		  
+		  driver.findElement(By.xpath(locatormap.get("Project$CreateProject_Button"))).click();
+		  
+		//  BrowserClose();
+	  result = driver.findElement(By.xpath(locatormap.get("Home$Logout_Link"))).isDisplayed();
+			
+			
+			
+			if (result)
+			{
+				System.out.println("Login_003 is passed");
+				writeResultsToFile("Login_003","PASS");
+			}
+			else
+			{
+				captureScreenShot("login_AT_003");
+				System.out.println("Login_003 is Failed!!");
+				writeResultsToFile("Login_003","FAILED!!!");
+			}
+		 }
+			catch(Exception e)
+			{
+				captureScreenShot("login_AT_003");
+				System.out.println("Login_003 is Failed!!");
+				writeResultsToFile("Login_003","FAILED!!!");
+			}
+			
+			
+		//	BrowserClose();
+	}
+	
+	
+	
+}
+
